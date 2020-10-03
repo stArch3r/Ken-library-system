7d409419fcaa23b5af9156f1d2bd9f0f7faf80ea
stArch3r <you@example.com>
2020-10-03 23:37:39 +0300

    Initial commit

---
+++ frontend/controllers/AssighnbookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+
+class AssighnbookController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionAssignbook()
+    {
+        $model = new \frontend\models\BorrowedBook();
+
+        if ($model->load(Yii::$app->request->post())&& $model->save()) {
+return $this->redirect(['site/index']);
+
+
+};
+
+        return $this->renderAjax('assignbook', [
+            'model' => $model,
+        ]);
+    }
+}
---
+++ frontend/views/assighnbook/assignbook.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use frontend\models\Student;
+use frontend\models\Book;
+use dosamigos\datepicker\DatePicker;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\BorrowedBook */
+/* @var $form ActiveForm */
+$students = ArrayHelper::map(Student::find()->all(), 'studentsId', 'fullName');
+$books = ArrayHelper::map(Book::find()->all(), 'bookId', 'bookName');
+?>
+<div class="assignbook">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'studentId')->dropDownList($students) ?>
+        <?= $form->field($model, 'bookId')->dropDownList($books) ?>
+        <?= $form->field($model, 'borrowDate')->widget(
+          DatePicker::className(), [
+           'inline' => false,
+           'clientOptions' => [
+             'autoclose' => true,
+             'format' => 'yyyy-mm-dd'
+           ]
+         ]);?>
+        <?= $form->field($model, 'returnDate')->widget(
+          DatePicker::className(), [
+           'inline' => false,
+           'clientOptions' => [
+             'autoclose' => true,
+             'format' => 'yyyy-mm-dd'
+           ]
+         ]);?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- assignbook -->
